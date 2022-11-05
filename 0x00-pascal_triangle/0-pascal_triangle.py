af842d3bbaedd1ae6e5cdbff46f51f4468e6e2bb
hyelda <hndzarma@gmail.com>
2022-11-05 20:12:16 +0100

    Adjust to pep8

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -4,6 +4,7 @@
 a module for creating a pascal triangle
 """
 
+
 def pascal_triangle(depth):
     """
     :input: triangle depth
@@ -13,16 +14,15 @@ def pascal_triangle(depth):
         return [] 
     elif depth == 1:
         return [[1]]
-    
+
     triangle = [[1], [1,1]]
 
     while len(triangle) < depth:
         temp_lst = [1]
         last_val = triangle[-1]
         for i in range(len(last_val) - 1):
-            temp_lst.append(last_val[i] + last_val[i+1])
-        
+            temp_lst.append(last_val[i] + last_val[i + 1])    
         temp_lst.append(1)
         triangle.append(temp_lst)
-    
+
     return triangle
